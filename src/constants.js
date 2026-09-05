4299b45da8612de29200cbf98b33c9b94cd3189b
Alexander James <aljames@google.com>
2026-09-05 15:14:41 +0000

    refactor(logger): upgrade logger to Logger class with debounced notification dispatcher

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,6 @@
 export const MODULE_ID = "bakana-action-display";
+export const MODULE_NAME = "Bakana's Action Display";
 export const MODULE_TLA = "BAD";
 export const GITHUB_ISSUES_URL = "https://github.com/aljames-arctic/bakana-action-display/issues";
+
 
