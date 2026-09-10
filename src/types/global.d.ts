c8716947daabfef33dca7aa94ca5a0d2ddf46aac
Alexander James <aljames@google.com>
2026-09-10 18:11:23 +0000

    refactor(types): make global.d.ts sole importer of systems.d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -86,6 +86,20 @@ declare global {
     [key: string]: unknown;
   }
 
+  type Dnd5eSkill = import('./systems.js').Dnd5eSkill;
+  type Dnd5eTool = import('./systems.js').Dnd5eTool;
+  type Dnd5eActivity = import('./systems.js').Dnd5eActivity;
+  type Dnd5eTraitData = import('./systems.js').Dnd5eTraitData;
+  type Dnd5eSensesData = import('./systems.js').Dnd5eSensesData;
+  type Actor5e = import('./systems.js').Actor5e;
+  type Item5e = import('./systems.js').Item5e;
+  type Pf1Skill = import('./systems.js').Pf1Skill;
+  type Pf1TraitData = import('./systems.js').Pf1TraitData;
+  type ActorPF = import('./systems.js').ActorPF;
+  type ItemPF = import('./systems.js').ItemPF;
+  type Pf2eStatistic = import('./systems.js').Pf2eStatistic;
+  type ActorPF2e = import('./systems.js').ActorPF2e;
+  type ItemPF2e = import('./systems.js').ItemPF2e;
 }
 
 declare module 'fvtt-types/configuration' {
